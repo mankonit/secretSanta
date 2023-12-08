8de060ed3650f22e3cd5b4e9ab65809f825ae6eb
mankonit <mankonit@gmail.com>
2023-12-08 17:58:03 +0100

    import

---
+++ app.py
@@ -0,0 +1,145 @@
+from flask import Flask, render_template, request, session, flash, send_from_directory, jsonify
+import os, sqlite3, random
+
+app = Flask(__name__)
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'87qsv54j7sd887qsd684788ts6df58dy8757s57d7jh7n7v7q8'
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/enrol/', methods=['GET', 'POST']) # type: ignore
+def enrol():
+    if state() == 0:
+        if request.method == 'GET':
+            return render_template('enrol.html', names = namesWithoutPseudo())
+        if request.method == 'POST':
+            l_realname = request.form['realname']
+            l_pseudo = request.form['pseudo']
+            try :
+                # l_query = str("INSERT INTO people ('name', 'pseudo') VALUES ('xxx_realname_xxx', 'xxx_pseudo_xxx')").replace('xxx_realname_xxx', l_realname).replace('xxx_pseudo_xxx', l_pseudo)
+                l_query = str("UPDATE people SET pseudo = 'xxx_pseudo_xxx' WHERE name = 'xxx_realname_xxx'").replace('xxx_realname_xxx', l_realname).replace('xxx_pseudo_xxx', l_pseudo)
+                db(l_query)
+            except:
+                app.logger.info('Query error')
+                flash('Pas possible, ma bonne dame...')
+                return render_template('enrol.html', names = namesWithoutPseudo())
+            else:
+                flash("C'est fait !")
+                return list()
+    else:
+        return render_template('notyet.html')
+
+@app.route('/list/')
+def list():
+    if state() == 0:
+        return render_template('list.html', persons = namesWithPseudo())
+    else:
+        return render_template('notyet.html')
+    
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    l_query = str("UPDATE people SET pseudo = NULL WHERE id = xxx_id_xxx").replace("xxx_id_xxx", str(id))
+    db(l_query)
+    return list()
+
+@app.route('/result/')
+def result():
+    if state() == 0:
+        return render_template('notyet.html')
+    else:
+        return render_template('result.html', persons = associations())
+
+
+@app.route('/admin/', methods=['GET', 'POST']) # type: ignore
+def admin():
+    return render_template('admin.html')
+
+@app.route('/do/<int:id>')
+def do(id):
+    app.logger.info("Id : " + str(id))
+    if id == 1:
+        boggle()
+        return render_template('admin.html')
+    
+def boggle():
+    if state() != 0:
+        return
+    l_query = "SELECT id, family FROM people"
+    l_liste = db(l_query)
+    app.logger.info("Avant boggle" + str(l_liste))
+
+    l_failed = True
+    while l_failed:
+        app.logger.info("Tentative" )
+        l_failed = False
+        # Créer une copie de la liste pour ne pas modifier l'originale
+        l_data_copy = l_liste.copy()
+        # Initialiser le dictionnaire pour stocker les associations id
+        l_allocations = {}
+        # Mélanger la liste de manière aléatoire
+        random.shuffle(l_data_copy)
+        # Associer les id de manière aléatoire en respectant la contrainte de groupe
+        for id, family in l_data_copy:
+            available_ids = [other_id for other_id, other_family in l_data_copy if other_family != family and other_id not in l_allocations.values()]
+            if available_ids:
+                allocated_id = random.choice(available_ids)
+                l_allocations[id] = allocated_id
+                l_data_copy = [(other_id, other_group) for other_id, other_group in l_data_copy if other_id != allocated_id]
+            else:
+                print("Impossible de respecter la contrainte de groupe.")
+                l_failed = True
+                break
+    
+
+    app.logger.info(l_allocations)
+    
+    for l_id in l_allocations.keys():
+        l_query = str("UPDATE people SET target = xxx_target_xxx WHERE id = xxx_id_xxx").replace("xxx_target_xxx", str(l_allocations[l_id])).replace("xxx_id_xxx", str(l_id))
+        db(l_query)
+
+    l_query = "UPDATE config SET value = 1 WHERE key = 'state'"
+    db(l_query)
+    
+    return
+
+def state():
+    l_query = "SELECT value FROM config WHERE key = 'state'"
+    return int(db(l_query)[0][0])
+
+def namesWithoutPseudo():
+    l_query = "SELECT id, name FROM people WHERE pseudo IS NULL ORDER BY name"
+    return db(l_query)
+
+def namesWithPseudo():
+    l_query = "SELECT id, name, pseudo FROM people WHERE pseudo IS NOT NULL ORDER BY name"
+    return db(l_query)
+
+def associations():
+    l_query = "SELECT pp1.pseudo, pp2.name FROM people pp1 INNER JOIN people pp2 ON pp1.target = pp2.id ORDER BY pp1.pseudo"
+    return db(l_query)
+
+def db(i_query):
+    app.logger.info(i_query)
+    con = sqlite3.connect("santa.db")
+    cur = con.cursor()
+    l_result = cur.execute(i_query).fetchall()
+    con.commit()
+    con.close()
+    return l_result
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
